75efc51ee15900e270e2d3a4d03551c8e3291a8f
Brian Scaturro <scaturrob@gmail.com>
2022-02-18 18:25:07 -0500

    chore: add build

---
+++ build.clj
@@ -0,0 +1,30 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'com.github.brianium/blah)
+
+(def class-dir "target/classes")
+
+(def basis (b/create-basis {:project "deps.edn"}))
+
+(def version (:version basis))
+
+(def jar-file (format "target/%s.jar" (name lib)))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src/cljs"]
+                :scm {:url                 "https://github.com/brianium/blah"
+                      :connection          "scm:git:git://github.com/brianium/blah.git"
+                      :developerConnection "scm:git:git://github.com/brianium/blah.git"
+                      :tag                 "HEAD"}})
+  (b/copy-dir {:src-dirs ["src/cljs"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
